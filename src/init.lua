d3203a69ee6270635ee592d18e9f632be0257805
Ale32bit <ale.proto00@gmail.com>
2017-07-09 12:39:30 +0200

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -2,8 +2,25 @@
   Osmium
   Init
 ]]--
+if osmium then
+    return
+end
+shell = nil
+_G.osmium = {}
+
+local nativeLoadfile = loadfile
+
+local nativeFS = {}
+
+for k,v in pairs(_G.fs) do
+    nativeFS[k] = v
+end
 
 local panicMode = false
+term.setBackgroundColor(colors.black)
+term.setTextColor(colors.white)
+term.clear()
+term.setCursorPos(1,1)
 
 local function panic(err) -- crash 
     panicMode = true
@@ -12,22 +29,73 @@ local function panic(err) -- crash
             task.kill(k)
         end
     end)
-    term.setBackgroundColor(colors.black)
+    term.setBackgroundColor(colors.red)
     term.setTextColor(colors.white)
     term.clear()
     term.setCursorPos(1,1)
-    printError(err)
+    print("Osmium")
+    print("")
+    print("Osmium errored and has been shut down")
+    print("")
+    print(err or "Unknown error")
+    print("")
+    print("Please report to the developers")
 end
 
 local function init() 
-    local ok, err = pcall(function() -- 
-        dofile("/rom/programs/shell.lua") --temp
-    end)
+    local ok, err = pcall(setfenv(nativeLoadfile("/.Osmium/appEngine.lua"),setmetatable({
+            nFS = nativeFS,
+        },{__index = getfenv()})))
     if not ok then
         panic(err)
     end
 end
 
+-- change os version
+
+function os.version()
+    return "Osmium 0.1"
+end
+
+-- Require function
+
+function osmium.require(lib)
+    if not lib then
+        return nil
+    end
+    libn = fs.getName(lib)
+    if nativeFS.exists("/.Osmium/libs/"..libn) and not nativeFS.isDir("/.Osmium/libs/"..libn) then
+        lib = "/.Osmium/libs/"..libn
+    elseif nativeFS.exists("/rom/apis/"..libn) and not nativeFS.isDir("/rom/apis/"..libn) then
+        lib = "/rom/apis/"..libn
+    elseif nativeFS.exists(lib) then
+        lib = lib -- really?!
+    elseif _G[lib] and type(_G[lib]) == "table" then
+        return _G[lib]
+    end
+    
+    local tEnv = {}
+    setmetatable(tEnv,{__index = _G})
+    local fnAPI, err = nativeLoadfile( lib, tEnv)
+    if fnAPI then
+        local ok, err = pcall(fnAPI)
+        if not ok then
+            printError(err)
+            return nil
+        end
+    else
+        printError(err)
+        return nil
+    end
+    
+    local tAPI = {}
+    for k,v in pairs( tEnv ) do
+        if k ~= "_ENV" then
+            tAPI[k] = v
+        end
+    end
+    return tAPI
+end
 
 -- task
 --[[
@@ -61,7 +129,7 @@ end
 function task.launch(fn, name)
   _proc[#_proc + 1] = {
     co = coroutine.create(setfenv(fn, getfenv())),
-    name = name or tostring(co):gsub("thread: ",""),
+    name = name or "lua",
   }
   return true
 end
@@ -102,4 +170,4 @@ end
 if panicMode then -- shows error
     os.pullEventRaw("key")
 end
-_G.term = nil -- force shutdown computer
+--_G.term = nil -- force shutdown computer
