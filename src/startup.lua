741f50595bf5512e2a30750eb0b22c821d913be4
Ale32bit <ale.proto00@gmail.com>
2017-07-11 17:39:50 +0200

    Update startup.lua

--- src/startup.lua
+++ src/startup.lua
@@ -1,9 +1,1 @@
-print("Osmium is still in development.")
-print("Would you like to start it?")
-print("Y/n")
-local r = read() --very advanced dev script
-if r == "" or r:lower() == "y" or r:lower() == "yes" then
-  os.run({},"/.Osmium/init.lua")
-else
-  print("Boot aborted")
-end
+os.run({},"/.Osmium/init.lua")
